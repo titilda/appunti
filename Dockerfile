1c67e2591a820f5096fbaf10b784df14bdedc2f4
TheManchineel <37479927+TheManchineel@users.noreply.github.com>
2024-12-11 20:13:48 +0100

    build(docker): come on, make it do something!

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,6 @@
 FROM pandoc/core:3.5
-ENV CHROME_BIN="/usr/bin/chromium-browser" \
-    PUPPETEER_SKIP_CHROMIUM_DOWNLOAD="true"
 USER root
-COPY build_docs.sh /root/build_docs.sh
-RUN apk add --update udev sudo bash git python3 ttf-freefont chromium npm && npm install -g mermaid-filter@1.4.5 --unsafe-perm=true && chmod +x /root/build_docs.sh
+COPY build_docs_v2.py /root/build_docs_v2.py
+RUN apk add --update sudo bash git python3 ttf-freefont && chmod +x /root/build_docs_v2.py
 WORKDIR /
 ENTRYPOINT ["/bin/bash"]
---
+++ build_docs_v2.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+import datetime
+import pathlib
+from subprocess import Popen
+
+# find files with .md extension min depth 2
+for path in pathlib.Path("/workspace").rglob("*/*.md"):
+    print(f"Processing {path}...")
+    # get the file's last modified time
+    last_modified = datetime.datetime.fromtimestamp(path.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S")
+    output_filename = path.parent / f"{path.stem}.html"
+    Popen(
+        [
+            "pandoc",
+            "--template",
+            "template.html",
+            "--toc",
+            "--katex=https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/",
+            "--metadata",
+            "toc-title=Indice",
+            "--metadata",
+            "lang=it",
+            "--metadata",
+            f"date={last_modified}",
+            "--metadata",
+            "maxwidth=100vw",
+            "--verbose",
+            "-o",
+            output_filename,
+            path,
+        ]
+    ).wait()
+
+print("Done! :)")
+# TODO: move index build process here
