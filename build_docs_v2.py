791a5605b6dffcac98525afc48eb3530d794d81d
TheManchineel <37479927+TheManchineel@users.noreply.github.com>
2024-12-12 11:50:44 +0100

    build: update GitHub workflows and improve document generation

--- build_docs_v2.py
+++ build_docs_v2.py
@@ -2,50 +2,44 @@
 import datetime
 import pathlib
 from subprocess import Popen
+from io import StringIO
 
-# find files with .md extension min depth 2
-for path in pathlib.Path("/workspace").rglob("*/*.md"):
-    print(f"Processing {path}...")
-    # get the file's last modified time
-    last_modified = datetime.datetime.fromtimestamp(path.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S")
-    output_filename = path.parent / f"{path.stem}.html"
-    Popen(
-        [
-            "pandoc",
-            "--template",
-            "template.html",
-            "--toc",
-            "--katex=https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/",
-            "--metadata",
-            "toc-title=Indice",
-            "--metadata",
-            "lang=it",
-            "--metadata",
-            f"date={last_modified}",
-            "--metadata",
-            "maxwidth=100vw",
-            "--metadata",
-            "margin-left=0",
-            "--metadata",
-            "margin-top=0",
-            "--metadata",
-            "margin-right=0",
-            "--metadata",
-            "margin-bottom=0",
-            "--metadata",
-            "padding-left=0",
-            "--metadata",
-            "padding-top=0",
-            "--metadata",
-            "padding-right=0",
-            "--metadata",
-            "padding-bottom=0",
-            "--verbose",
-            "-o",
-            output_filename,
-            path,
-        ]
-    ).wait()
 
-print("Done! :)")
-# TODO: move index build process here
+def build_docs_and_index():
+    with StringIO() as index_entries:
+        for path in pathlib.Path("/workspace").rglob("*/*.md"):
+            print(f"Processing {path}...")
+            last_modified = datetime.datetime.fromtimestamp(path.stat().st_mtime).strftime("%Y-%m-%d %H:%M:%S")
+            output_filename = path.parent / f"{path.stem}.html"
+            index_entries.write(f'<li><a href="{output_filename}">{path.parent.name}</a></li>\n')
+            Popen(
+                [
+                    "pandoc",
+                    "--template",
+                    "template.html",
+                    "--toc",
+                    "--katex=https://cdn.jsdelivr.net/npm/katex@0.16.8/dist/",
+                    "--metadata",
+                    "toc-title=Indice",
+                    "--metadata",
+                    "lang=it",
+                    "--metadata",
+                    f"date={last_modified}",
+                    "--metadata",
+                    "maxwidth=100vw",
+                    "--verbose",
+                    "-o",
+                    output_filename,
+                    path,
+                ]
+            ).wait()
+
+        with open("/workspace/index.html", "w+") as index_file:
+            original_html = index_file.read()
+            index_file.write(original_html.replace("{{{PLACEHOLDER}}}", index_entries.getvalue()))
+
+
+if __name__ == "__main__":
+    build_docs_and_index()
+
+print("Done building docs! :)")
