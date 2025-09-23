5a7b7217e68a88aada6546cefce2db2b269a0cf1
XedaGmr <andrea.lunghi3@gmail.com>
2025-09-23 14:17:34 +0200

    feat: restructure app layout

---
+++ app/layouts/default.vue
@@ -0,0 +1,20 @@
+<template>
+    <div class="min-h-screen flex flex-col">
+        <UHeader title="TiTilda Appunti" to="/">
+            <template #title>
+                <span class="flex items-center gap-2">
+                    <img class="h-6 w-auto" src="/logo.svg" alt="TiTilda Logo">
+                    TiTilda Appunti
+                </span>
+            </template>
+        </UHeader>
+        <div class="flex-grow">
+            <slot />
+        </div>
+        <UFooter>
+            <template #left>
+            <p class="text-muted text-sm">Copyright © {{ new Date().getFullYear() }}</p>
+            </template>
+      </UFooter>
+    </div>
+</template>
