23c317069762ac4329121310a9a7fe7b1a10d471
XedaGmr <andrea.lunghi3@gmail.com>
2025-09-23 09:14:42 +0200

    feat: add Tailwind CSS integration and configuration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,15 @@
+import tailwindcss from "@tailwindcss/vite";
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-07-15',
+  compatibilityDate: "2025-07-15",
   devtools: { enabled: true },
 
-  modules: [
-    '@nuxt/content',
-    '@nuxt/eslint',
-    '@nuxt/image',
-    '@nuxt/ui'
-  ]
-})
+  css: ["~/assets/style/main.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ["@nuxt/content", "@nuxt/eslint", "@nuxt/image", "@nuxt/ui"],
+});
